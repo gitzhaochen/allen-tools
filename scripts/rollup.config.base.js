7d81155b218299a5216f06c2c4bf311abbe58ac2
zhaochen <zc17755112311@gmail.com>
2020-03-01 02:01:25 +0800

    chore: 1.0.13 changelog [ci skip]

--- scripts/rollup.config.base.js
+++ scripts/rollup.config.base.js
@@ -2,7 +2,8 @@ import pkg from '../package.json'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
-import vue from 'rollup-plugin-vue'
+import vue from 'rollup-plugin-vue2'
+import postcss from 'rollup-plugin-postcss'
 import { terser } from 'rollup-plugin-terser'
 const isProduction = process.env.NODE_ENV === 'production'
 export default {
@@ -10,12 +11,13 @@ export default {
   plugins: [
     resolve(),
     commonjs({ include: 'node_modules/**' }),
+    vue(),
+    postcss(),
     babel({
       runtimeHelpers: true,
       exclude: 'node_modules/**'
     }),
     isProduction && terser(),
-    vue()
   ],
   external(id) {
     // 对babel-runtime corejs等进行external
